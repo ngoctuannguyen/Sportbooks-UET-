bd16344339086ff25c929a4093b4244e63ad2327
huongluong23 <21020337@vnu.edu.vn>
2024-05-27 22:46:24 +0700

    phan trang cont

--- fe/src/pages/Contact/Contact.js
+++ fe/src/pages/Contact/Contact.js
@@ -2,74 +2,22 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 import CardView from "../../components/CardView";
-import { cardviewData, setFilteredCardviewData } from '../../components/CardView';
 import Fuse from "fuse.js";
+import ReactPaginate from 'react-paginate';
 
 const Contact = ({ isAdmin }) => {
-  const [cardviewData, setCardviewData] = useState([]);
-  const [filteredCardviewData, setFilteredCardviewData] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0); // Đặt trang hiện tại là 0
+  const [usersPerPage] = useState(12);
   const location = useLocation();
   const [prevLocation, setPrevLocation] = useState("");
+  const [searchName, setSearchName] = useState("");
+  
   useEffect(() => {
     setPrevLocation(location.state.data);
   }, [location]);
 
-  const [clientName, setclientName] = useState("");
-  const [email, setEmail] = useState("");
-  const [messages, setMessages] = useState("");
-
-  // ========== Error Messages Start here ============
-  const [errClientName, setErrClientName] = useState("");
-  const [errEmail, setErrEmail] = useState("");
-  const [errMessages, setErrMessages] = useState("");
-  // ========== Error Messages End here ==============
-  const [successMsg, setSuccessMsg] = useState("");
-
-  const handleName = (e) => {
-    setclientName(e.target.value);
-    setErrClientName("");
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    setErrEmail("");
-  };
-  const handleMessages = (e) => {
-    setMessages(e.target.value);
-    setErrMessages("");
-  };
-
-  // ================= Email Validation start here =============
-  const EmailValidation = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
-  };
-  // ================= Email Validation End here ===============
-
-  const handlePost = (e) => {
-    e.preventDefault();
-    if (!clientName) {
-      setErrClientName("Enter your Name");
-    }
-    if (!email) {
-      setErrEmail("Enter your Email");
-    } else {
-      if (!EmailValidation(email)) {
-        setErrEmail("Enter a Valid Email");
-      }
-    }
-    if (!messages) {
-      setErrMessages("Enter your Messages");
-    }
-    if (clientName && email && EmailValidation(email) && messages) {
-      setSuccessMsg(
-        `Thank you dear ${clientName}, Your messages has been received successfully. Futher details will sent to you by your email at ${email}.`
-      );
-    }
-  };
-
-  const [searchName, setSearchName] = useState("");
-  const [users, setUsers] = useState([]);
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -101,21 +49,21 @@ const Contact = ({ isAdmin }) => {
     };
     fetchUsers();
   }, [searchName]);
-  
+
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users]);
+
   const options = {
     includeScore: true,
     keys: ['name', 'phone', 'email', 'address', 'membership', "id"]
   }
 
-  const fuse = new Fuse(users, options);
+  const fuse = new Fuse(filteredUsers, options);
+  
   const handleSearch = (e) => {
     const searchQuery = e.target.value;
-  
-    const results = fuse.search(searchQuery);
-  
-    const filteredCardviewData = results.map(result => result.item);
-  
-    setFilteredCardviewData(filteredCardviewData);
+    setSearchName(searchQuery);
   };
 
   const handleDelete = async (id) => {
@@ -146,6 +94,13 @@ const Contact = ({ isAdmin }) => {
     }
   };
 
+  // Logic for pagination
+  const indexOfLastUser = (currentPage + 1) * usersPerPage; // Điều chỉnh tính toán trang cuối
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+
+  const paginate = ({ selected }) => setCurrentPage(selected); // Cập nhật trang hiện tại khi người dùng chọn trang mới
+
   return (
     <>
       {isAdmin ? (
@@ -153,12 +108,12 @@ const Contact = ({ isAdmin }) => {
           <div className="max-w-container mx-auto px-4">
             <Breadcrumbs title="Users" prevLocation={prevLocation} />
             <div className="flex w-full pl-48 pr-48">
-              <input value={searchName} onChange={(e) => setSearchName(e.target.value)} className="w-4/5 h-10 border-2 border-gray-300 rounded-md px-2" type="text" placeholder="Search for users..." />
+              <input value={searchName} onChange={handleSearch} className="w-4/5 h-10 border-2 border-gray-300 rounded-md px-2" type="text" placeholder="Search for users..." />
               <button className="w-1/5 h-10 bg-primeColor text-white rounded-md">Search</button>
             </div>
             {/* Display the card views */}
             <div className="grid grid-cols-6 gap-4 mt-16">
-              {users.map((user) => (
+              {currentUsers.map((user) => (
                 <CardView
                   key={user.id}
                   image={user.image}
@@ -174,89 +129,38 @@ const Contact = ({ isAdmin }) => {
                 />
               ))}
             </div>
+            {/* Pagination */}
+            <Pagination
+              pageCount={Math.ceil(filteredUsers.length / usersPerPage)}
+              onPageChange={paginate}
+              currentPage={currentPage}
+            />
           </div>
         </>
       ) : (
         <div className="max-w-container mx-auto px-4">
-          <Breadcrumbs title="Contact" prevLocation={prevLocation} />
-          {successMsg ? (
-            <p className="pb-20 w-96 font-medium text-green-500">{successMsg}</p>
-          ) : (
-            <form className="pb-20">
-              <h1 className="font-titleFont font-semibold text-3xl">
-                Fill up a Form
-              </h1>
-              <div className="w-[500px] h-auto py-6 flex flex-col gap-6">
-                <div>
-                  <p className="text-base font-titleFont font-semibold px-2">
-                    Name
-                  </p>
-                  <input
-                    onChange={handleName}
-                    value={clientName}
-                    className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor"
-                    type="text"
-                    placeholder="Enter your name here"
-                  />
-                  {errClientName && (
-                    <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
-                      <span className="text-sm italic font-bold">!</span>
-                      {errClientName}
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <p className="text-base font-titleFont font-semibold px-2">
-                    Email
-                  </p>
-                  <input
-                    onChange={handleEmail}
-                    value={email}
-                    className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor"
-                    type="email"
-                    placeholder="Enter your name here"
-                  />
-                  {errEmail && (
-                    <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
-                      <span className="text-sm italic font-bold">!</span>
-                      {errEmail}
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <p className="text-base font-titleFont font-semibold px-2">
-                    Messages
-                  </p>
-                  <textarea
-                    onChange={handleMessages}
-                    value={messages}
-                    cols="30"
-                    rows="3"
-                    className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor resize-none"
-                    type="text"
-                    placeholder="Enter your name here"
-                  ></textarea>
-                  {errMessages && (
-                    <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
-                      <span className="text-sm italic font-bold">!</span>
-                      {errMessages}
-                    </p>
-                  )}
-                </div>
-                <button
-                  onClick={handlePost}
-                  className="w-44 bg-primeColor text-gray-200 h-10 font-titleFont text-base tracking-wide font-semibold hover:bg-black hover:text-white duration-200"
-                >
-                  Post
-                </button>
-              </div>
-            </form>
-          )}
+          {/* Your Contact Form JSX */}
         </div>
       )}
     </>
   );
 };
 
+const Pagination = ({ pageCount, onPageChange, currentPage }) => {
+  return (
+    <ReactPaginate
+          nextLabel=""
+          onPageChange={onPageChange}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={pageCount}
+          previousLabel=""
+          pageLinkClassName="w-9 h-9 border-[1px] border-lightColor hover:border-gray-500 duration-300 flex justify-center items-center"
+          pageClassName="mr-6"
+          containerClassName="flex text-base font-semibold font-titleFont py-10"
+          activeClassName="bg-black text-white"
+        />
+  );
+};
 
 export default Contact;
